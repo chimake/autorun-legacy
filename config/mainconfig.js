547ad6d4996886b2befbeff4108acc5f54f9de7e
Chimake <dev.chimaobi@gmail.com>
2022-11-13 01:36:47 +0100

    Configuration

--- config/mainconfig.js
+++ config/mainconfig.js
@@ -1,23 +1,23 @@
 export const MainConfig = {
     AppDetails: {
-        app_name: "Your App Name", 
-        app_description: "Your App Description Here",
-        app_identifier: "com.something.app",
+        app_name: "Autorun", 
+        app_description: "On-demand logistics service.",
+        app_identifier: "com.martcity.autorun",
         ios_app_version: "1.0.0", 
         android_app_version: 100
     },
     FirebaseConfig: {
-        apiKey: "XXXXXXXXXXXXXXXXX",
-        authDomain: "XXXXXXXXXXXXXXXXX",
-        databaseURL: "XXXXXXXXXXXXXXXXX",
-        projectId: "XXXXXXXXXXXXXXXXX",
-        storageBucket: "XXXXXXXXXXXXXXXXX",
-        messagingSenderId: "XXXXXXXXXXXXXXXXX",
-        appId: "XXXXXXXXXXXXXXXXX",
-        measurementId: "XXXXXXXXXXXXXXXXX"
+        apiKey: "AIzaSyAXUi2qR_qDCs25UJYSesUnaiaMF2t0RA0",
+        authDomain: "autorun-d9ee6.firebaseapp.com",
+        databaseURL: "https://autorun-d9ee6-default-rtdb.firebaseio.com",
+        projectId: "autorun-d9ee6",
+        storageBucket: "autorun-d9ee6.appspot.com",
+        messagingSenderId: "997032681699",
+        appId: "1:997032681699:web:0fcffbfd13be4dc5f2a5d6",
+        measurementId: "G-CD71K3JHNQ"
     },
-    Google_Map_Key: "XXXXXXXXXXXXXXXXX",
-    facebookAppId: "XXXXXXXXXXXXXXXXX",
+    Google_Map_Key: "AIzaSyA1VnSwYKngTeN8s7HZ2V0tGcCVAHLD9IE",
+    facebookAppId: "838153504165436",
     PurchaseDetails: {
         CodeCanyon_Purchase_Code: "",
         Buyer_Email_Address: ""
